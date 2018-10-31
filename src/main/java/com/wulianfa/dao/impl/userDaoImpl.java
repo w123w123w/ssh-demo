85158aabb4d6b97cad909831d2b12bbc0667e64d
wulianfa <511525550@qq.com>
2018-10-31 23:27:31 +0800

    commit！

--- src/main/java/com/wulianfa/dao/impl/userDaoImpl.java
+++ src/main/java/com/wulianfa/dao/impl/userDaoImpl.java
@@ -2,6 +2,7 @@ package com.wulianfa.dao.impl;
 
 import com.wulianfa.dao.userDao;
 import com.wulianfa.model.User;
+import org.hibernate.LockMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
@@ -22,20 +23,20 @@ public class userDaoImpl implements userDao {
     }
 
     //查找用户
-    public User queryUser(User user){
+    public User queryUser(User user) {
+        hibernateTemplate.load(User.class, user.getUserId(), LockMode.UPGRADE_NOWAIT);//悲观锁
         String hql = "from User where userId = ?";
         List<User> users = (List<User>)hibernateTemplate.find(hql, user.getUserId());
-        User u = users.get(0);
-        return u;
+        return users.get(0);
     }
 
     //修改用户
-    public void updateUser(User user){
+    public void updateUser(User user) {
         hibernateTemplate.update(user);
     }
 
     //删除用户
-    public void deleteUser(User user){
+    public void deleteUser(User user) {
         hibernateTemplate.delete(user);
     }
 
